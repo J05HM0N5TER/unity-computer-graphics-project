5d92e3fe6ada0eb3899c513ecb4c4ea777040831
Josh Francis <joshua.francis@live.com.au>
2020-03-11 15:59:24 +1100

    Added simple animation
    The animation just switches between two animations every couple seconds.

---
+++ Assets/Scripts/TestAnimation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAnimation : MonoBehaviour
+{
+	public float timer_delay = 10;
+	private float timer = 0;
+	bool is_idle = false;
+	Animator ani;
+	// Start is called before the first frame update
+	void Start()
+	{
+		ani = GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		timer += Time.deltaTime;
+		
+		if (timer > timer_delay)
+		{
+			is_idle = !is_idle;
+			timer = 0;
+			if (is_idle)
+			{
+				ani.SetTrigger("runTransition");
+			}
+			else if (!is_idle)
+			{
+				ani.SetTrigger("idleTransition");
+			}
+		}
+	}
+}
