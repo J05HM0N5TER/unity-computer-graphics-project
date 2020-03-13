b7c8c1606df3acf875eb8e924e98e7fb8321be21
Joshua Francis <joshua.francis@live.com.au>
2020-03-13 15:56:14 +1100

    Added button to toggle run animation.

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+	public Button runToggle = null;
+	public Animator charmeleon = null;
+	private bool isRunning = false;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		if (!runToggle)
+		{
+			Debug.LogError(nameof(runToggle) + " is not set");
+		}
+		else
+		{
+			// Set runToggleOnClick to run when button is pressed
+			runToggle.onClick.AddListener(runToggleOnClick);
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+
+	/// <summary>
+	/// Runs when runToggle button is clicked
+	/// </summary>
+	void runToggleOnClick()
+	{
+		if (isRunning)
+		{
+			charmeleon.SetTrigger("idleTransition");
+		}
+		else
+		{
+			charmeleon.SetTrigger("runTransition");
+		}
+		isRunning = !isRunning;
+	}
+}
